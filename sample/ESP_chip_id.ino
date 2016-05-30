0a40a8b662b6e7c9db7c6ae18214f7f76cd9c1ae
Axon Active Vietnam <aavn-github@axonactive.com>
2016-05-30 13:52:39 +0700

    Update ESP_chip_id.ino

--- sample/ESP_chip_id.ino
+++ sample/ESP_chip_id.ino
@@ -1,1 +1,15 @@
+#include <ESP8266WiFi.h>
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Starting \n");
+  int chipId = 0;
+  chipId = ESP.getChipId();
+  Serial.print("Chip ID: " );
+  Serial.println(chipId);
+}
 
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
